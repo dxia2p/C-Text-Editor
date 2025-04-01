eec0b4548e52e7fafe00f8d182253092e6f0177a
David <d.xia111@gmail.com>
2025-03-31 22:16:49 -0600

    Now displays tildes for lines past eof

--- ceditor.c
+++ ceditor.c
@@ -3,6 +3,7 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
@@ -10,26 +11,33 @@
 #define CTRL_KEY(k) ((k) & 0x1f)  // A macro to turn alphabet key codes into their CTRL counterparts
 
 /* data */
+struct editorConfig {
+  int screenrows;
+  int screencols;
+  struct termios orig_termios;  // Stores the original terminal settings so we can restore the user's terminal!
+};
 
-struct termios orig_termios;  // Stores the original terminal settings so we can restore the user's terminal!
-
+struct editorConfig E;
 /* terminal */
 
 void die(const char *s) {
+  write(STDOUT_FILENO, "\x1b[2J", 4);  // Clears screen
+  write(STDOUT_FILENO, "\x1b[H", 3);  // Puts cursor at top left
+
   perror(s);
   exit(1);
 }
 
 void disableRawMode(){
-  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
     die("tcsetattr");
 }
 
 void enableRawMode() {  // Enables "raw" mode, which allows us to read the user's inputs right away instead of after they press enter
-  if(tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
+  if(tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) die("tcgetattr");
   atexit(disableRawMode);
 
-  struct termios raw = orig_termios;
+  struct termios raw = E.orig_termios;
   raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON );  // Disables Ctrl-S and Ctrl-Q, and also carriage returns (Ctrl-m)
   raw.c_oflag &= ~(OPOST);  // Turns off translating "\n" to "\r\n" in the terminal's output
   raw.c_cflag |= (CS8);  // Sets character size to 8 bits
@@ -50,10 +58,34 @@ char editorReadKey(){  // Waits for a keypress then returns it
   return c;
 }
 
+int getWindowSize(int *rows, int *cols){  // Use pointers in the arguments to "return" multiple values (this also lets us use the return for error codes
+  struct winsize ws;
+
+  if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+    return -1;  // Report error
+  } else {
+    *cols = ws.ws_col;
+    *rows = ws.ws_row;
+    return 0;
+  }
+}
+
 /* output */
 
+void editorDrawRows(){
+  int y;
+  for (y = 0; y < E.screenrows; y++){
+    write(STDOUT_FILENO, "~\r\n", 3);  // Write tildes for lines after end of file
+  }
+}
+
 void editorRefreshScreen() {
-  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[2J", 4);  // writes an escape sequence that clears the screen
+  write(STDOUT_FILENO, "\x1b[H", 3);  // Positions cursor at top left of screen using the H command
+
+  editorDrawRows();
+
+  write(STDOUT_FILENO, "\x1b[H", 3);
 }
 
 /* input */
@@ -63,6 +95,9 @@ void editorProcessKeypress(){  // Waits for a keypress, then handles it.
 
   switch (c) {
     case CTRL_KEY('q'):
+      // We don't use atexit() to clear the screen when program exits so that the error message printed by die() doesn't get erased
+      write(STDOUT_FILENO, "\x1b[2J", 4);
+      write(STDOUT_FILENO, "\x1b[H", 3);
       exit(0);
       break;
   }
@@ -70,8 +105,13 @@ void editorProcessKeypress(){  // Waits for a keypress, then handles it.
 
 /* init */
 
+void initEditor() { // Initializes all fields in the E struct except termios
+  if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
+}
+
 int main(){
   enableRawMode();
+  initEditor();
 
   char c;
   while (1){
